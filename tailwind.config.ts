448de52c4eb879047d5b35fdbc9a662ec50744e4
Abdul Wahab <wahab@craftyzen.com>
2024-10-06 10:16:34 +0600

    add daisyUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,19 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui"
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+        },
     },
-  },
-  plugins: [],
+    daisyui: {
+        themes: ["light", "dark", "cupcake"],
+    },
+    plugins: [daisyui],
 };
 export default config;
